0fb925d49c66c347239cbdec36aeca17d8d37961
Aman-ABJIMA <aman.singh@abjima.com>
2023-01-05 18:49:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+           // Console.WriteLine("Hello, World!");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webHost => { webHost.UseStartup<Startup>(); });
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp2
+{
+    public class Startup
+    {
+     public void ConfigureServices(IServiceCollection services)
+     {
+
+     }
+
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                
+            });
+        }
+    }
+}
